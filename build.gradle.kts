cf05bab610478a1c31ad50b524eedbf8abdd7a17
Matthijs van den Bos <matthijs@vandenbos.org>
2020-12-02 15:08:56 +0100

    Enable ING versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,9 @@ plugins {
 
 if (!extra.has("kspVersion")) {
     val kotlinBaseVersion: String by project
+    val ingVersion: String by project
     val today = LocalDateTime.now().format(DateTimeFormatter.BASIC_ISO_DATE)
-    extra.set("kspVersion", "$kotlinBaseVersion-dev-experimental-$today-ING")
+    extra.set("kspVersion", "$kotlinBaseVersion-dev-experimental-$today-$ingVersion")
 }
 
 subprojects {
